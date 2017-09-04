04a8a3f26538b5aa95a096dac0c8bf363b04d2e1
Alexander-Blair <zanderb@hotmail.co.uk>
2017-09-04 11:43:38 +0100

    Withdraw method added and tested. Also cannot withdraw or deposit negative amount

--- lib/account.rb
+++ lib/account.rb
@@ -5,12 +5,31 @@ class Account
   end
 
   def deposit(amount)
+    raise "Cannot deposit negative amount" if negative_amount?(amount)
     increase_balance(amount)
+  end
+
+  def withdraw(amount)
+    raise "Cannot withdraw negative amount" if negative_amount?(amount)
+    decrease_balance(amount)
   end
 
   private
 
   def increase_balance(amount)
     @balance += amount
+  end
+
+  def decrease_balance(amount)
+    raise "Insufficient funds" if insufficient_funds?(amount)
+    @balance -= amount
+  end
+
+  def negative_amount?(amount)
+    amount < 0
+  end
+
+  def insufficient_funds?(amount)
+    amount > @balance
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,10 @@ require 'account'
 describe Account do
   subject(:account) { described_class.new }
   let(:deposit_amount) { 100 }
+  let(:withdraw_amount) { 25 }
+  let(:insufficient_funds_error) { "Insufficient funds" }
+  let(:negative_deposit_error) { "Cannot deposit negative amount" }
+  let(:negative_withdrawal_error) { "Cannot withdraw negative amount" }
 
   describe "#initialize" do
     it "begins with a balance of 0" do
@@ -14,6 +18,25 @@ describe Account do
     before { account.deposit(deposit_amount) }
     it "increases the account balance by the specified amount" do
       expect(account.balance).to eq deposit_amount
+    end
+    it "must be a positive number" do
+      expect{ account.deposit(-deposit_amount) }.to raise_error negative_deposit_error
+    end
+  end
+
+  describe "#withdraw" do
+    before do
+      account.deposit(deposit_amount)
+      account.withdraw(withdraw_amount)
+    end
+    it "decreases the account balance by the specified amount" do
+      expect(account.balance).to eq deposit_amount - withdraw_amount
+    end
+    it "doesn't allow withdrawal in excess of balance" do
+      expect{ account.withdraw(account.balance + 1) }.to raise_error insufficient_funds_error
+    end
+    it "must be a positive number" do
+      expect{ account.withdraw(-withdraw_amount) }.to raise_error negative_withdrawal_error
     end
   end
 end
