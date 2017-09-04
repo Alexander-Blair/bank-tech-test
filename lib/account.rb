d6c74ccb7975e122c18c7925a275f2fd42e5a1c7
Alexander-Blair <zanderb@hotmail.co.uk>
2017-09-04 11:22:40 +0100

    Test added, and passed, for making a deposit

--- lib/account.rb
+++ lib/account.rb
@@ -3,4 +3,14 @@ class Account
   def initialize
     @balance = 0
   end
+
+  def deposit(amount)
+    increase_balance(amount)
+  end
+
+  private
+
+  def increase_balance(amount)
+    @balance += amount
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,10 +2,18 @@ require 'account'
 
 describe Account do
   subject(:account) { described_class.new }
+  let(:deposit_amount) { 100 }
 
   describe "#initialize" do
     it "begins with a balance of 0" do
       expect(account.balance).to eq 0
+    end
+  end
+
+  describe "#deposit" do
+    before { account.deposit(deposit_amount) }
+    it "increases the account balance by the specified amount" do
+      expect(account.balance).to eq deposit_amount
     end
   end
 end
